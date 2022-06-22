30b6ae0fb6a0870684343f1b534a000eadfd9409
NurRizqiMaulana <maulana29.rizqi@gmail.com>
2022-06-22 21:13:52 +0700

    Initial commit

---
+++ src/BinaryTree/BinaryTree.java
@@ -0,0 +1,12 @@
+package BinaryTree;
+
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree(int key){this.root=new Node(key);
+    }
+    public BinaryTree(){
+        this.root=null;
+    }
+
+}
